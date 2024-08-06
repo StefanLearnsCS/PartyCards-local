5223f1cbe36745677dddb90063f46260e8b1cfea
Stefan <stefancool00@gmail.com>
2024-08-06 10:18:12 -0700

    Created front end for login and register functions

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Container, Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import axios from "axios";
+
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    const data = {username: username, password: password};
+    axios.post("http://localhost:3001/auth/login", data).then((response) => {
+      console.log(response.data);
+    })
+  };
+
   return (
-    <div>
-      Login
-    </div>
+    <Container className='col-xl-4 col-lg-6 col-md-7 rounded border border-secondary' id='create-pack-container'>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label> Username </Form.Label>
+          <Form.Control 
+            placeholder="Username" 
+            onChange={(event) => {setUsername(event.target.value);}}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label> Password </Form.Label>
+          <Form.Control 
+            placeholder="Password"
+            onChange={(event) => {setPassword(event.target.value);}} />
+        </Form.Group>
+        <Button variant="primary" onClick={login} id="create-pack-button"> Login </Button>
+      </Form>
+    </Container>
   )
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,69 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+import { Container, Button, Form as BootstrapForm} from 'react-bootstrap';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Register() {
+
+  const initialValues = {
+    username:"",
+    password:""
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().min(3).max(15).required(),
+    password: Yup.string().min(4).max(20).required(),
+  });
+
+  const BootstrapField = ({ field, form: { touched, errors }, ...props }) => (
+      <BootstrapForm.Group controlId={field.name}>
+          <BootstrapForm.Label>{props.label}</BootstrapForm.Label>
+          <BootstrapForm.Control
+              {...field}
+              {...props}
+              isInvalid={touched[field.name] && errors[field.name]}
+          />
+          <ErrorMessage name={field.name} component="div" className="invalid-feedback" />
+      </BootstrapForm.Group>
+  );
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/auth", data).then(() => {
+      console.log(data);
+    })
+  };
+
   return (
-    <div>
-      Register
-    </div>
+    <Container className='col-xl-4 col-lg-6 col-md-7 rounded border border-secondary' id='create-pack-container'>
+      <Formik 
+          initialValues={initialValues} 
+          onSubmit={onSubmit} 
+          validationSchema={validationSchema} 
+          validateOnChange={true}
+          validateOnBlur={true}
+      >
+          <Form>
+              <Field
+                  name="username"
+                  type="text"
+                  label="Username:"
+                  placeholder="Username"
+                  component={BootstrapField}
+              />
+              <Field
+                  name="password"
+                  type="password"
+                  label="Password:"
+                  placeholder="Password"
+                  component={BootstrapField}
+              />
+
+              <Button variant="primary" type="submit" id="create-pack-button"> Register </Button>
+          </Form>
+      </Formik>             
+    </Container>
   )
 }
 
